d1672c3bc2815f59e12f9513802129bf3af497f9
david <david@server.home>
2014-12-03 01:16:09 +0000

    added random words

--- src/anagrama/Palavra.java
+++ src/anagrama/Palavra.java
@@ -5,26 +5,39 @@
  */
 package anagrama;
 
+import java.util.Random;
+
 /**
  *
  * @author david
  */
 public class Palavra {
 
-    public static String doGerarNovaPalavra() {
+    public static String[] doGerarNovaPalavra() {
         int max = Lista.lista.length;
-        int num = (int) (Math.random()*(max));
-        String palavra = Lista.lista[num];
-        
+        String[] palavra = new String[2];
+        Random r = new Random();
+        int num = new Random().nextInt(max);
+        palavra[0] = (Lista.lista[num]);
+        palavra[1] = (doAlterarLetras(r, palavra[0]));
         return palavra;
     }
-    private static String doAlterarLetras(String palavra){
+
+    private static String doAlterarLetras(Random random, String palavra) {
+//@author Rastikan http://stackoverflow.com/a/20589105
         
-        System.out.println();
-         
-        
-       
-        
-        return palavra;
+        // Convert your string into a simple char array:
+        char a[] = palavra.toCharArray();
+
+        // Scramble the letters using the standard Fisher-Yates shuffle, 
+        for (int i = 0; i < a.length - 1; i++) {
+            int j = random.nextInt(a.length - 1);
+            // Swap letters
+            char temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+        return new String(a);
+
     }
 }
